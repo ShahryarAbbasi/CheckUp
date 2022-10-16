c4eee1f4f051bde7f6d4b07e638afb9205afe112
ShahryarAbbasi <shizzleabbasi@gmail.com>
2022-10-16 12:20:03 -0400

    added player form

---
+++ checkup/src/components/EditForm.js
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+const BASE_URL =  "https://checkupbackend.herokuapp.com/";
+
+  function EditForm(props) {
+
+    const navigate = useNavigate()
+    const [editForm, setEditForm] = useState(null);
+    const params = useParams()
+    const gameId = params.gameId
+    const URL = BASE_URL + `games/${gameId}`  
+
+    const getGame = async () => {
+        const response = await fetch(URL)
+        const data = await response.json()
+        setEditForm(data)
+    }
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const output = JSON.stringify(editForm)
+            const options = {
+                method: "PUT",
+                headers: {
+                    "Content-Type" : "application/json"
+                },
+                body: output
+            }
+            const response = await fetch(URL, options)
+            const responseData = await response.json()
+            console.log(responseData)
+            navigate(`/games/${gameId}`)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleChange = (e) => {
+        const data = { ...editForm, [e.target.name]: e.target.value }
+        setEditForm(data)
+    }
+
+    useEffect (() => {
+        getGame()
+    }, [])
+
+    return (
+        <>
+            {editForm ?
+            <>
+           <form className="form" onSubmit={handleSubmit}>
+           <h1>Edit Game</h1>
+            <label>
+            Where: <input type="text" required name="location" placeholder="Enter book name" onChange={handleChange} value={editForm.location} />
+            </label>
+            <br></br>
+            <label>
+            When: <input type="text" required name="time" placeholder="http://..." onChange={handleChange} value={editForm.time} />
+            </label>
+            </form>
+           </> : null}
+        </>
+    )
+}
+
+export default EditForm
